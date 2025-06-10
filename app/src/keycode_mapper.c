98322b28f7ad051787ccc3a0b6873895108b83bb
47LeCoste <82815207+grugnoymeme@users.noreply.github.com>
2025-06-11 01:57:55 +0200

    Update keycode_mapper.c

--- app/src/keycode_mapper.c
+++ app/src/keycode_mapper.c
@@ -1,8 +1,12 @@
+#include <stdint.h>
+
 char map_keycode_to_char(uint8_t* keycode) {
     if (keycode[0] >= 4 && keycode[0] <= 29) {
-        return 'a' + (keycode[0] - 4); // Map 'a' to 'z'
+        return 'a' + (keycode[0] - 4); // a-z
     } else if (keycode[0] >= 30 && keycode[0] <= 38) {
-        return '0' + (keycode[0] - 30); // Map '1' to '9' and '0'
+        return '1' + (keycode[0] - 30); // 1-9
+    } else if (keycode[0] == 39) {
+        return '0'; // keycode 39 è 0
     }
-    return '?'; // Unknown key
+    return '?'; // carattere non riconosciuto
 }
