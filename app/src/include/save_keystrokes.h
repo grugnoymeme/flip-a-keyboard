487290f4b9d9442ba3b395e58c00c67314d2ad24
47LeCoste <82815207+grugnoymeme@users.noreply.github.com>
2025-06-11 02:02:30 +0200

    Update save_keystrokes.h

--- app/src/include/save_keystrokes.h
+++ app/src/include/save_keystrokes.h
@@ -3,10 +3,6 @@
 
 #include <stdint.h>
 
-/**
- * Salva un singolo carattere nel file keystrokes.txt sulla SD.
- * @param character Il carattere da salvare (es. 'a', 'B', '1', ecc.)
- */
 void save_keystroke_to_storage(char character);
 
 #endif // SAVE_KEYSTROKES_H
